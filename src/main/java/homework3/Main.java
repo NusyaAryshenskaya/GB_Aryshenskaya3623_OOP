0de36de8c9097a1e6ea938acfe037d711a00c5d0
ZHenya <saltykovazh@mail.ru>
2023-06-26 21:05:37 +0400

    Homework3

--- src/main/java/homework3/Main.java
+++ src/main/java/homework3/Main.java
@@ -8,11 +8,11 @@ public class Main {
         StudentController controller = new StudentController(service);
 
         System.out.println("\nСписок без сортировки:");
-        controller.addStudent(new Student(4L, "Иван Федоров"));
-        controller.addStudent(new Student(6L, "Полина Иванова"));
-        controller.addStudent(new Student(2L, "Феофан Сидоров"));
-        controller.addStudent(new Student(1L, "Терентий Попов"));
-        controller.addStudent(new Student(8L, "Евлампия Субботина"));
+        controller.addStudent(new Student(4L, "Алексей Сидоров"));
+        controller.addStudent(new Student(6L, "Полина Федулова"));
+        controller.addStudent(new Student(2L, "Даниил Вотяков"));
+        controller.addStudent(new Student(1L, "Вдадимир Поясов"));
+        controller.addStudent(new Student(8L, "Татьяна Селиванова"));
         controller.printAll();
 
         System.out.println("\nСортировка по имени:");
